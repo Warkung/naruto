30e28ccc7cee7183c41bb0a85f6d98cdf9485b16
warkung <warkung@gmail.com>
2024-10-17 15:41:18 +0700

    first upload

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "about",
+      element: <div>About</div>,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
